b4dfb2d3820af7119519f29cf06c2365825c5106
873657660 <873657660@qq.com>
2019-10-29 22:17:49 +0800

    sku

--- gmall-user-manage/src/test/java/com/atguigu/gmall/usermanage/GmallUsermanageApplicationTests.java
+++ gmall-user-manage/src/test/java/com/atguigu/gmall/usermanage/GmallUsermanageApplicationTests.java
@@ -1,13 +1,24 @@
 package com.atguigu.gmall.usermanage;
 
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class GmallUsermanageApplicationTests {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class GmallUsermanageApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() throws IOException {
+        List list = new ArrayList<>();
+        List list2 = new LinkedList();
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String s = input.readLine();
+        System.out.println(s);
     }
 
 }
